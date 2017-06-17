28a781152029e77517486c6691243db91263e984
JC (Joshua Caleb) Dy <jcdy@berkeley.edu>
2017-06-16 21:49:04 -0700

    Transparent nav bar turns black when srolling down

--- assets/js/jquery.js
+++ assets/js/jquery.js
@@ -1,8 +1,10 @@
-$(window).scroll(function() {
-	if ($document.scrollTop() > 50) {
-		$('nav').addClass('shrink');
-	}
-	else {
-		$('nav').removeClass('shrink');
-	}
+$(document).ready(function() {
+	$(window).scroll(function() {
+		if($(document).scrollTop() > 20) {
+			$('#navbar').addClass('shrink');
+		}
+		else {
+			$('#navbar').removeClass('shrink');
+		}
+	});
 });
