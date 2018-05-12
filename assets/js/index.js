e483e91928d6a8c17d75388aeefc6a61995d71d7
JC (Joshua Caleb) Dy <jcdy@berkeley.edu>
2018-05-12 03:24:33 -0700

    Added stamps and links to outside pages, fixed passport color scheme

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 $(".stamp").hover(function() {
     $(this).find("img").fadeTo(400, 1);
 	}, function() {
-    $(this).find("img").fadeTo(400, 0.7);
+    $(this).find("img").fadeTo(400, 0.5);
 });
 
 })
